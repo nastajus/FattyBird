4d82e946231206040ed5fff3e7d513e3327a07a5
Ian Nastajus <ian.nastajus@gmail.com>
2016-07-14 00:13:06 -0400

    de le Zack, goes to absolute rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,8 +17,18 @@ public class Movement : MonoBehaviour {
 	}
 	
 	void Update () {
+        Turn();
         MoveForward();
 
+    }
+
+    void Turn()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        print(horiz + " : " + vert);
+        //set forward vect directly for now, lerp later
+        transform.forward = GetVectFromAngles(horiz, vert);
     }
 
     void MoveForward()
@@ -26,16 +36,21 @@ public class Movement : MonoBehaviour {
         transform.Translate(0, 0, speed * Time.deltaTime);
     }
 
-    float GetAngleFromVect2D(Vector2 vector)
+    public static float GetAngleFromVect2D(Vector2 vector)
     {
-        float y = vector.y;
         float x = vector.x;
+        float y = vector.y;
+        float angleDegrees = GetAngleFromComponents2D(x, y);
+        return angleDegrees;
+    }
+    public static float GetAngleFromComponents2D(float x, float y)
+    {
         float angleRadians = Mathf.Atan2(y, x);
         float angleDegrees = angleRadians * Mathf.Rad2Deg;
         return angleDegrees;
     }
 
-    Vector2 RotateVector2D(Vector2 vector, float angleAdd)
+    public static Vector2 RotateVector2D(Vector2 vector, float angleAdd)
     {
         float angleCurrent = GetAngleFromVect2D(vector);
         float angleNext = angleCurrent + angleAdd;
@@ -43,15 +58,31 @@ public class Movement : MonoBehaviour {
         return vectorNext;
     }
 
-    Vector2 GetVectFromAngle2D(float angle)
+    public static Vector2 GetVectFromAngle2D(float angle, float length = 1f)
     {
-        float fraction = Mathf.Tan(angle);
-        float x = fraction;
-        float y = 1;
-		Vector2 vector = (new Vector2(x, y)).normalized;
+        float opposite = length * Mathf.Sin(angle);
+        float adjacent = length * Mathf.Cos(angle);
+        Vector2 vector = new Vector2(adjacent, opposite);
         return vector;
     }
-
+    public static Vector3 RotateVector(Vector3 vector, float angleHoriz, float angleVert, float? length = null)
+    {
+        float angleCurrentHoriz = GetAngleFromComponents2D(vector.x, vector.y);
+        float angleCurrentVert = GetAngleFromComponents2D(vector.z, vector.y);
+        float len = length.HasValue ? length.Value : vector.magnitude;
+        Vector3 vectorNext = GetVectFromAngles(angleCurrentHoriz, angleCurrentVert).normalized * len;
+        return vectorNext;
+    }
+    public static Vector3 GetVectFromAngles(float angleHoriz, float angleVert, float length = 1f)
+    {
+        Vector2 horizVect = GetVectFromAngle2D(angleHoriz);
+        Vector2 vertVect = GetVectFromAngle2D(angleVert);
+        Vector3 result = horizVect;
+        result.y += vertVect.x;
+        result.z += vertVect.y;
+        result = result.normalized * length;
+        return result;
+    }
 
     //float GetAngleFromVect(Vector3 vector)
     //{
